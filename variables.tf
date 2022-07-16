0eea11229596ab154399800d53485893b9faba9c
Jitendra Singh Lodhi <63745741+jeetuapth@users.noreply.github.com>
2022-07-16 21:43:32 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+# define GCP project name
+variable "project" {
+  type = string
+  description = "GCP project name"
+  default = "jitendralab"
+}
+
+# define GCP region
+variable "region" {
+  type = string
+  description = "GCP region"
+  default     = null
+}
+# define GCP zone
+variable "zone" {
+  type = string
+  description = "GCP zone"
+  default     = null
+}
+/*
+# define private subnet
+variable "private_subnet_cidr_1" {
+  type = string
+  description = "private_subnet_CIDR 1"
+  default     = null
+}
+*/
+  
+variable "private-subnet" {
+  description = "Create private subnets with these names"
+  type        = list(string)
+  default     = null
+}
+variable "private_subnet_cidr" {
+  description = "Create private subnets cidr with these names"
+  type        = list(string)
+  default     = null
+  
+# define application name
+variable "app_name" {
+  type = string
+  description = "Application name"
+  default     = null
+}
